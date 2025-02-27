9a439fd6519aadfaf6ca1c9917ee65c85f52b138
Josemiles <josephotai209@gmail.com>
2025-02-27 10:40:55 +0300

    Starting with the basic dashboard design with other placeholders

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Dashboard from './Dashboard/Dashboard.jsx'
+import Header from './Header.jsx'
+//import Calendar from './Calendar.jsx'
+import Footer from './Footer.jsx'
+function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <Footer />
+    </>
+  )
+
+}
+
+export default App
---
+++ my-react-app/src/Calendar.jsx
@@ -0,0 +1,21 @@
+import Fullcalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import iterationPlugin from '@fullcalendar/interaction'
+
+function Calendar() {
+    return (<div>
+        <Fullcalendar
+            plugins={[dayGridPlugin, timeGridPlugin, iterationPlugin]}
+            initialView={"dayGridMonth"}
+            headerToolbar={{
+                start: "today,preview,next",
+                center: "title",
+                end: "dayGridMonth,timeGridWeek,timeGridDay"
+            }
+            }
+            height={"auto"}
+        />
+    </div>)
+}
+export default Calendar
