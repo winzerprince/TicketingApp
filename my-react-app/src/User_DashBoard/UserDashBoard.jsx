29f1af901e2f4c7f830a473ebbc4685c1befeda7
ssentongoJoshua <ssentongoj05hu4@gmail.com>
2025-02-27 23:28:54 +0300

    Created a .jsx file for the user dashboard and made it the base page

---
+++ my-react-app/src/User_DashBoard/UserDashBoard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function UserDashBoard() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="dashboard">
+        <header className="header">
+          <h1>MOVIETIX</h1>
+          <nav className="nav">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/movies">Movies</a>
+              </li>
+              <li>
+                <a href="/book-online">Book Online</a>
+              </li>
+              <li>
+                <a href="/theaters">Theaters</a>
+              </li>
+              <li>
+                <button onClick={() => navigate("/Dashboard")}>
+                  AdminDashBoard
+                </button>
+              </li>
+              <li>
+                <a href="/search">Search</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main className="main-content">
+          <section className="welcome-section">
+            <h2>Welcome to the hub of entertainment</h2>
+            <p>
+              Where you can easily book your movie tickets online and indulge in
+              a seamless movie experience with family and friends.
+            </p>
+            <button className="get-tickets-btn">Get Tickets</button>
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default UserDashBoard;
